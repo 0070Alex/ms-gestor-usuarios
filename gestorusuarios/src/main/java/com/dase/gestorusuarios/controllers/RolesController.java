08bef78104097fd42ae08c3f8cc53d22f23cad97
alex <78516248+0070Alex@users.noreply.github.com>
2022-10-01 08:26:01 -0600

    Se versiona el paquete del proyecto

---
+++ gestorusuarios/src/main/java/com/dase/gestorusuarios/controllers/RolesController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dase.gestorusuarios.controllers;
+
+import com.dase.gestorusuarios.commons.CommonController;
+import com.dase.gestorusuarios.entities.Roles;
+import com.dase.gestorusuarios.services.RolesSvc;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Alexander Elias
+ */
+@RestController
+@RequestMapping(path = "/roles")
+public class RolesController extends CommonController<Roles, RolesSvc>{
+    
+}
---
+++ gestorusuarios/src/main/java/com/dase/gestorusuarios/controllers/UsuariosController.java
@@ -0,0 +1,30 @@
+package com.dase.gestorusuarios.controllers;
+
+import com.dase.gestorusuarios.commons.CommonController;
+import com.dase.gestorusuarios.entities.Usuarios;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import java.util.Locale;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.dase.gestorusuarios.services.UsuariosSvc;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+@RequestMapping(path = "/user")
+public class UsuariosController extends CommonController<Usuarios, UsuariosSvc>{
+    
+
+    @GetMapping(path = "/byUserAndPassword/{user}/{pass}")
+    @ApiOperation(value = "Consulta usuarios por medio de su username y password")
+    public ResponseEntity<?> findUserByUsernameAndPass(@PathVariable(required = true) @ApiParam(value = "user") String user, @PathVariable(required = true) @ApiParam(value = "pass") String pass, @RequestHeader(name = "Accept-Languaje", required = false) Locale locale) {
+        return ResponseEntity.ok().body(service.findUserByUsernameAndPass(user, pass));
+    }
+
+}
